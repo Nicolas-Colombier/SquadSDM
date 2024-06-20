f7feefaab59316b5f66be04c4e6e17448b8b92ae
Nicolas-Colombier <nicolas.colombier@oclock.school>
2024-06-20 16:52:51 +0200

    🔨 refactor: Remove specific delete commands from utility
    The delete commands that remove specific bot and server commands were eliminated from the deleteCommandsUtils.js file. More simplicity was desired and the utility of these features was reassessed leading to their removal. Retained the operation that deletes all existing bot commands for utility.

--- utils/deleteCommandsUtils.js
+++ utils/deleteCommandsUtils.js
@@ -3,26 +3,6 @@ import config from '../config.json' assert { type: "json" };
 
 const rest = new REST().setToken(config.token);
 
-// Mettre en commentaire les commandes que vous ne souhaitez pas exécuter
-
-// Supprime une commande spécifique du serveur
-rest.delete(Routes.applicationGuildCommand(config.clientId, config.guildId, 'commandId'))
-    .then(() => console.log('Suppression réussie de la commande du serveur.'))
-    .catch(console.error);
-
-
-// Supprime toutes les commandes existantes dans le serveur
-rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: [] })
-    .then(() => console.log('Suppression réussie de toutes les commandes du serveur.'))
-    .catch(console.error);
-
-
-// Supprime une commande spécifique du bot
-rest.delete(Routes.applicationCommand(config.clientId, 'commandId'))
-    .then(() => console.log('Suppression réussie de la commande du bot.'))
-    .catch(console.error);
-
-
 // Supprime toutes les commandes existantes du bot
 rest.put(Routes.applicationCommands(config.clientId), { body: [] })
     .then(() => console.log('Suppression réussie de toutes les commandes du bot.'))
